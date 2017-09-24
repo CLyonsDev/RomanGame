7a228f8ac487b9ca1bf62fa91a0403aaf761845e
Collin Lyons <collinclyons@gmail.com>
2017-09-23 18:50:18 -0700

    Animations and a fun little cutscene
    Press "w" to activate

---
+++ Assets/Scripts/CoolCombatScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolCombatScene : MonoBehaviour {
+
+    public Transform attacker;
+    public Transform dyer;
+    public ParticleSystem bloodPS;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.W))
+        {
+            StartCoroutine(DoCoolCutscene());
+        }
+	}
+
+    private IEnumerator DoCoolCutscene()
+    {
+        attacker.GetComponentInChildren<Animator>().SetTrigger("RunAttk");
+        yield return new WaitForSeconds(1.6f);
+        bloodPS.Play();
+        dyer.GetComponentInChildren<Animator>().SetTrigger("Die");
+    }
+}
