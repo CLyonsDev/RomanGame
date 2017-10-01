23f52711221ce309fc092781de023f509c8e7ba5
Tony Simmering <tonysimmering@gmail.com>
2017-10-01 15:40:55 -0700

    dsez

--- Assets/TPMovement.cs
+++ Assets/TPMovement.cs
@@ -4,19 +4,54 @@ using UnityEngine;
 
 public class TPMovement : MonoBehaviour {
 
-    Animator anim;
-
-	// Use this for initialization
-	void Start () {
-        anim = transform.GetComponentInChildren<Animator>();
+	public float rotationDamping = 20f;
+	public float runSpeed = 10f;
+	public int gravity = 20;
+	public float jumpSpeed = 8;
+	bool canJump;
+	float moveSpeed;
+	float verticalVel;  // Used for continuing momentum while in air    
+	CharacterController controller;
+	void Start()
+	{
+		controller = (CharacterController)GetComponent(typeof(CharacterController));
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        anim.SetFloat("Horiz", Input.GetAxis("Horizontal"));
-        anim.SetFloat("Vert", Input.GetAxis("Vertical"));
-        transform.GetChild(0).Rotate(transform.up, Input.GetAxis("Horizontal") * 55 * Time.deltaTime);
-        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + (transform.GetChild(0).transform.forward * 3f * Time.deltaTime * Input.GetAxis("Vertical")));
+	float UpdateMovement()
+	{
+		// Movement
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+		Vector3 inputVec = new Vector3(x, 0, z);
+		inputVec *= runSpeed;
+		controller.Move((inputVec + Vector3.up * -gravity + new Vector3(0, verticalVel, 0)) * Time.deltaTime);
+		// Rotation
+		if (inputVec != Vector3.zero)
+			transform.rotation = Quaternion.Slerp(transform.rotation, 
+				Quaternion.LookRotation(inputVec), 
+				Time.deltaTime * rotationDamping);
+		return inputVec.magnitude;
+	}
+	void Update()
+	{
+		// Check for jump
+		if (controller.isGrounded )
+		{
+			canJump = true;
+			if ( canJump &amp;&amp; Input.GetKeyDown("space") )
+			{
+				// Apply the current movement to launch velocity
+				verticalVel = jumpSpeed;
+				canJump = false;
+			}
+		}else
+		{           
+			// Apply gravity to our velocity to diminish it over time
+			verticalVel += Physics.gravity.y * Time.deltaTime;
+		}
+		// Actually move the character
+		moveSpeed = UpdateMovement();  
+		if ( controller.isGrounded )
+			verticalVel = 0f;// Remove any persistent velocity after landing
+	}
 
-    }
 }
